333f8258fee19e42290599eef5d5d5cbb36d4414
djumanov <djumanovdev@gmail.com>
2025-11-03 17:49:24 +0500

    add tasks

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from uuid import uuid4
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+task_list = []
+
+def add_task(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return render(request=request, template_name='add.html')
+    elif request.method == 'POST':
+        form = request.POST
+
+        title = form.get('title')
+        description = form.get('description')
+        priority = form.get('priority')
+        status = form.get('status')
+        deadline = form.get('deadline')
+
+        # validation
+        if title == '':
+            return HttpResponse("Titleni unitdingiz.", status=400)
+        elif priority not in ['low', 'medium', 'high']:
+            return HttpResponse("priorityni unitdingiz.", status=400)
+        elif status not in ['todo', 'inprogress', 'done']:
+            return HttpResponse("statusni unitdingiz.", status=400)
+        elif deadline == '':
+            return HttpResponse("deadlineni unitdingiz.", status=400)
+        else:
+            new_task = {
+                'id': str(uuid4()),
+                'title': title,
+                'description': description,
+                'priority': priority,
+                'status': status,
+                'deadline': deadline,
+            }
+            task_list.append(new_task)
+
+            return HttpResponse('ok')
+    
